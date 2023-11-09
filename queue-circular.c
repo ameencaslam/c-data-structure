8ae853fb26c9f06b49e4da8640e5790e6013dd21
ameencaslam <120704619+ameencaslam@users.noreply.github.com>
2023-11-09 19:55:25 +0530

    Add files via upload

---
+++ queue-circular.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#define size 10
+int i,x,front=-1,rear=-1,ch;
+int q[size];
+
+void enq()
+{
+    if((front%size)-(rear%size)==1)
+    {
+        printf("\nError!, Overflow\n");
+    }
+    else
+    {
+        rear=((rear+1)%size);
+        printf("Enter the Element : ");
+        scanf("%d",&q[rear]);
+    }
+}
+
+void deq()
+{
+    if((rear%size)-(front%size)==0)
+    {
+        printf("\nError!, Underflow\n");
+    }
+    else
+    {
+        front=((front+1)%size);
+    }
+}
+
+void display()
+{
+    if((rear%size)-(front%size)==0)
+    {
+        printf("\nError!, Empty Queue\n\n");
+    }
+    else
+    {
+        printf("\n");
+        for(i=(front%size)+1;i<=(rear%size);i++)
+        {
+            printf("\t%d",q[i]);
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    do
+    {
+        printf("\n1. ENQUEUE\n");
+        printf("2. DEQUEUE\n");
+        printf("3. DISPLAY\n");
+        printf("4. END\n");
+        printf("Enter your Choice : ");
+        scanf("%d",&ch);
+
+        switch(ch)
+        {
+            case 1:
+            enq();
+            break;
+
+            case 2:
+            deq();
+            break;
+            
+            case 3:
+            display();
+            break;
+
+            case 4:
+            break;
+            
+            default:
+            printf("\nInvalid Option\n");
+        }
+    }while(ch!=4);
+
+    printf("\nByeBye...\n\n.");
+}
